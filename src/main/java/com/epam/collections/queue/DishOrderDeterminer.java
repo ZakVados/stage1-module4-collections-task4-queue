26f26e09ea234d2ae1c63ceaed93c34f8d548b51
ZakVados <c0H7W2c3#mPZD$2Iuc4B0LbnruPGwi>
2023-01-04 23:56:15 +0200

    DishOrderDeterminer task done

--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,27 @@
 package com.epam.collections.queue;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        List<Integer> dishes = new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
+        for (int i = 1; i <= numberOfDishes; i++) {
+            dishes.add(i);
+        }
+        int counter = 0;
+        while (dishes.size() != 0) {
+            for (int i = 0; i < dishes.size(); i++) {
+                counter++;
+                if (counter == everyDishNumberToEat) {
+                    res.add(dishes.get(i));
+                    dishes.remove(i);
+                    counter = 0;
+                    i -= 1;
+                }
+            }
+        }
+        return res;
     }
 }
