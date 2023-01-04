99751397b0c9171530133ba30ac521540e7b0a47
ZakVados <c0H7W2c3#mPZD$2Iuc4B0LbnruPGwi>
2023-01-04 21:12:38 +0200

    ArrayDeque creator done

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,32 @@
 package com.epam.collections.queue;
 
-import java.util.ArrayDeque;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> res = new ArrayDeque<>();
+        boolean isStart = true;
+        Integer card;
+
+        while (firstQueue.size() != 0) {
+            if (!isStart) {
+                card = res.pollLast();
+                if (card != null) firstQueue.add(card);
+            }
+            for (int i = 0; i < 2; i++) {
+                card = firstQueue.poll();
+                if (card != null) res.add(card);
+            }
+            if (!isStart) {
+                card = res.pollLast();
+                if (card != null) secondQueue.add(card);
+            }
+            for (int i = 0; i < 2; i++) {
+                card = secondQueue.poll();
+                if (card != null) res.add(card);
+            }
+            isStart = false;
+        }
+        return res;
     }
 }
